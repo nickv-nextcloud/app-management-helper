8b82e64b2cf8af28cd0abf495b707dc68afb41e7
Joas Schilling <coding@schilljs.com>
2022-04-15 00:30:35 +0200

    Add init script which clones
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # app-management-helper
 A repository that helps doing things in many apps in one go
+
+## Steps
+1. Run `init-repositories.sh`
+2. Use the matching `run-*.sh`, if none exist it's `run-for-all.sh $SCRIPT_NAME`
---
+++ init-repositories.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#
+
+mkdir Branched
+cd Branched/
+for REPOSITORY in activity \
+                  circles \
+                  files_pdfviewer \
+                  files_rightclick \
+                  files_videoplayer \
+                  firstrunwizard \
+                  logreader \
+                  nextcloud_announcements \
+                  notifications \
+                  password_policy \
+                  photos \
+                  privacy \
+                  recommendations \
+                  serverinfo \
+                  support \
+                  survey_client \
+                  text \
+                  viewer
+do
+    URL="git@github.com:nextcloud/$REPOSITORY.git"
+    if [[ "$REPOSITORY" = "support" ]]; then
+    	URL="git@github.com:nextcloud-gmbh/$REPOSITORY.git"
+    fi
+
+    git clone $URL
+done
+cd ../
+
+mkdir Stable
+cd Stable/
+for REPOSITORY in backup \
+                  data_request \
+                  deck \
+                  end_to_end_encryption \
+                  external \
+                  files_accesscontrol \
+                  files_automatedtagging \
+                  files_fulltextsearch \
+                  files_retention \
+                  flow_notifications \
+                  fulltextsearch \
+                  fulltextsearch_elasticsearch \
+                  groupfolders \
+                  impersonate \
+                  ldap_contacts_backend \
+                  ldap_write_support \
+                  profiler \
+                  richdocuments \
+                  sharepoint \
+                  spreed \
+                  terms_of_service \
+                  user_migration \
+                  user_retention \
+                  user_usage_report \
+                  workflow_pdf_converter \
+                  workflow_script
+do
+    URL="git@github.com:nextcloud/$REPOSITORY.git"
+
+    git clone $URL
+done
+cd ../
+
+mkdir Multibranch
+cd Multibranch/
+for REPOSITORY in announcementcenter \
+                  bruteforcesettings \
+                  calendar \
+                  contacts \
+                  files_antivirus \
+                  files_downloadactivity \
+                  files_downloadlimit \
+                  files_texteditor \
+                  files_zip \
+                  forms \
+                  globalsiteselector \
+                  guests \
+                  integration_discourse \
+                  integration_github \
+                  integration_gitlab \
+                  integration_google \
+                  integration_jira \
+                  integration_mastodon \
+                  integration_moodle \
+                  integration_reddit \
+                  integration_twitter \
+                  integration_whiteboard \
+                  integration_zammad \
+                  jsloader \
+                  mail \
+                  notify_push \
+                  officeonline \
+                  preferred_providers \
+                  quota_warning \
+                  ransomware_protection \
+                  registration \
+                  social \
+                  suspicious_login \
+                  talk_matterbridge \
+                  twofactor_totp \
+                  twofactor_u2f \
+                  twofactor_webauthn \
+                  user_oidc \
+                  user_saml
+do
+    URL="git@github.com:nextcloud/$REPOSITORY.git"
+
+    git clone $URL
+done
+cd ../
