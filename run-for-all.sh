9ad326224691d1fd9c1db3cb093a643d418c6abe
Joas Schilling <coding@schilljs.com>
2026-02-06 09:36:24 +0100

    fix(run-for-all): Should also include Enterprise/
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- run-for-all.sh
+++ run-for-all.sh
@@ -77,3 +77,26 @@ do
     fi
 done
 cd ../
+
+cd Enterprise/
+for dir in ./*/
+do
+    dir=${dir%*/}
+    if [[ "$SKIP_UNTIL" = "${dir##*/}" ]]; then
+        STILL_SKIP="0"
+    fi
+
+    if [[ "$STILL_SKIP" = "0" ]]; then
+        echo "#"
+        echo "#"
+        echo "# ${dir##*/}"
+        echo "#"
+        echo "#"
+        cd ${dir##*/}
+        ../../$SCRIPT_NAME.sh
+        cd ..
+    else
+        echo "Skipping ${dir##*/}"
+    fi
+done
+cd ../
